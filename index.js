1c8eb964a9d5f568d9d95bfa60f2f3e323aef109
sarath-dbug <sarathpattambi2013@gmail.com>
2023-11-21 12:25:59 +0530

    complet lastweek-hosting  commit message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://sarathpattambi2013:1hq758CYy1jSZNmT@timekeepers.pii0r7t.mongodb.net/?retryWrites=true&w=majority', {
+
+mongoose.connect('mongodb+srv://sarathpattambi2013:j5KG1IpftZ10zwMl@cluster0.mamtkjm.mongodb.net/timekeepes?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -11,15 +12,18 @@ mongoose.connect('mongodb+srv://sarathpattambi2013:1hq758CYy1jSZNmT@timekeepers.
   console.error('Error connecting to MongoDB:', error.message);
 });
 
-const express = require('express');
-const app = express();
+
+const express = require("express")
+const app = express()
 
 app.use(express.static('public'));
 
-const userRouter = require('./routes/userRouter');
-const adminRouter = require('./routes/adminRouter');
+const userRouter =require('./routes/userRouter');
+app.use('/',userRouter);    
 
-app.use('/', userRouter);
-app.use('/admin', adminRouter);
+const adminRouter =require('./routes/adminRouter');
+app.use('/admin',adminRouter);
 
-app.listen(8080, () => console.log('Server Running at http://localhost:8080'));
+
+app.listen(8080,()=>console.log("Server Running at http://localhost:8080'"))
+
